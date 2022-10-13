a0969f2bd3846e6d436b817524c0bc4824fc2e78
Aymen <aymenbouazra994@gmail.com>
2022-10-13 16:45:34 +0100

    create company component

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": 0
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
--- src/views/pages/company/CreateCompany.js
+++ src/views/pages/company/CreateCompany.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from "react"
 
 function CreateCompany() {
+  const [companyForm, setCompany] = useState({
+    companyName:'',
+    companyDescription:'',
+    email:'',
+    password:'',
+    role:'Admin',
+    photo:''
+  })
+  
+  const onFileSelect = ()=> {
+
+  }
+  const handleChange = (e) => {
+    const {id,value} = e.target
+  }
+  const handleSubmit = (e) => {
+
+  }
   return (
-    <div>CreateCompany</div>
+    <div className="card">
+      <div className="card-header">
+        <h3>Create company</h3>
+      </div>
+      <div className="card-body">
+        <form className="form-group" onSubmit={handleSubmit}>
+          <label>Company"s name:</label>
+          <input type="text" id="companyName" onChange={handleChange}className="form-control mb-4" placeholder="Name" />
+          <label>Description:</label>
+          <input type="text" id="companyDescription" onChange={handleChange}className="form-control mb-4" placeholder="Description" />
+          <label>E-mail:</label>
+          <input type="email" id="email" onChange={handleChange}className="form-control mb-4" placeholder="E-mail" />
+          <label>Password:</label>
+          <input type="password" id="password" onChange={handleChange}className="form-control mb-4" placeholder="Password" />
+          <label>Photo:</label>
+          <input type="file" onChange={onFileSelect} className="form-control mb-4" />
+          <button className="btn btn-primary px-5" type="submit">Create</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
